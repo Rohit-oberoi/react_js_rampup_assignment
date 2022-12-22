82754d51d1bdb3e13747129f1997c3b5e5431d17
007sunny <sunny.singhal@icanbwell.com>
2022-12-22 17:40:52 +0530

    Added loader

--- src/app/components/User.tsx
+++ src/app/components/User.tsx
@@ -4,34 +4,40 @@ import { useParams } from 'react-router-dom';
 import { UserType } from "../interfaces/UserType";
 import { DEMO_USER_DATA, CURRENT_USER } from '../constants/Constants';
 import useFetch from "../hooks/useFetch";
+import Loader from "../Loader";
 
 export const User = (props: { username: string; }): JSX.Element => {
     const [response, setResponse] = useState<UserType>(DEMO_USER_DATA);
+    const [loader, setLoader] = useState(true);
     let { username } = useParams();
-    const data: UserType = useFetch(username? username:props.username);
+    const data: UserType = useFetch(username? username:props.username, setLoader);
 
     useEffect(() => {
         setResponse(data);
     }, [data]);
 
     return (
-        <div className="mx-auto mt-3" style={{width: "300px"}}>
-            <div className="card" style={{width: "18rem"}}>
-                <img className="card-img-top" src={response.avatar_url} alt="User Profile Pic" />
-                <div className="card-body">
-                    <h5 className="card-title">
-                        <a href={response.html_url} className="card-link">{response.login}</a>
-                    </h5>
-                    {response.bio?<p className="card-text">{response.bio}</p>:''}
+        <>
+        {loader ? <Loader /> :
+            <div className="mx-auto mt-3" style={{width: "300px"}}>
+                <div className="card" style={{width: "18rem"}}>
+                    <img className="card-img-top" src={response.avatar_url} alt="User Profile Pic" />
+                    <div className="card-body">
+                        <h5 className="card-title">
+                            <a href={response.html_url} className="card-link">{response.login}</a>
+                        </h5>
+                        {response.bio?<p className="card-text">{response.bio}</p>:''}
+                    </div>
+                    <ul className="list-group list-group-flush">
+                        {response.email?<li className="list-group-item active">Email: {response.email}</li>:''}
+                        <li className="list-group-item">Following: {response.following}</li>
+                        <li className="list-group-item">Followers: {response.followers}</li>
+                        {response.location?<li className="list-group-item">Location: {response.location}</li>:''}
+                    </ul>
                 </div>
-                <ul className="list-group list-group-flush">
-                    {response.email?<li className="list-group-item active">Email: {response.email}</li>:''}
-                    <li className="list-group-item">Following: {response.following}</li>
-                    <li className="list-group-item">Followers: {response.followers}</li>
-                    {response.location?<li className="list-group-item">Location: {response.location}</li>:''}
-                </ul>
             </div>
-        </div>
+        }
+        </>
     )
 }
 
--- src/app/hooks/useFetch.tsx
+++ src/app/hooks/useFetch.tsx
@@ -1,16 +1,17 @@
 import axios from 'axios';
-import { useEffect, useState } from "react";
+import { SetStateAction, useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import { DEMO_USER_DATA, FETCH_USER_URL } from '../constants/Constants';
 import { ROUTES } from '../constants/routeConstants'; 
 import { UserType } from '../interfaces/UserType';
 
-const useFetch = (username: string): UserType => {
+const useFetch = (username: string, setLoader: { (value: SetStateAction<boolean>): void; (arg0: boolean): void; }): UserType => {
     const [data, setdata] = useState(DEMO_USER_DATA);
     const navigate = useNavigate();
 
     useEffect(() => {
-        axios.get(`${FETCH_USER_URL}${username}`).then(data => setdata(data.data))
+        setLoader(true);
+        axios.get(`${FETCH_USER_URL}${username}`).then((data) => {setdata(data.data); setLoader(false);})
         .catch(()=>{navigate(ROUTES.USER_NOT_FOUND, {state: { username: username }})});
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [username]);
